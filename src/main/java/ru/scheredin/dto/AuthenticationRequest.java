c7e39e5f220418117bd4329e89477d22f42b72a9
Artem <artem.scheredin@vk.team>
2024-02-20 18:10:08 +0300

    fixes

--- src/main/java/ru/scheredin/dto/AuthenticationRequest.java
+++ src/main/java/ru/scheredin/dto/AuthenticationRequest.java
@@ -1,10 +1,12 @@
 package ru.scheredin.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @RequiredArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 public class AuthenticationRequest {
